3b8ae32241a9d1c53d4a6558eec7b0c24f0f82cd
Marc-Antoine B <115265331+HazeFury@users.noreply.github.com>
2023-06-08 23:45:04 +0200

    added some components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Blog from "./pages/Blog";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <CssBaseline />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Blog" element={<Blog />} />
+      </Routes>
     </div>
   );
 }
